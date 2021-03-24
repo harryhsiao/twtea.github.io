9c90e2d62972deef89fe5e5c7b5cd6f710e92823
harryhsiao <58318398+harryhsiao@users.noreply.github.com>
2021-03-24 16:12:02 +0800

    製作側邊分類選項

--- src/components/page/cart.vue
+++ src/components/page/cart.vue
@@ -5,8 +5,17 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-4">
-          <div class="list-group" v-for="item in categorys" :key="item.id">
-            <a href="#" class="list-group-item list-group-item-action">{{item.category}}</a>
+          <div
+            class="list-group"
+            v-for="(item, index) in categorys"
+            :key="index"
+          >
+            <a
+              href="#"
+              class="list-group-item list-group-item-action"
+              @click.prevent="changeoption"
+              >{{ item }}</a
+            >
           </div>
         </div>
         <div class="col-sm-8">
@@ -14,7 +23,7 @@
             <div class="row">
               <div
                 class="col-sm-6 col-lg-4 mb-3"
-                v-for="item in custproducts"
+                v-for="item in filtersdata"
                 :key="item.id"
               >
                 <router-link :to="{ path: `/product/${item.id}` }">
@@ -33,7 +42,7 @@
                 </router-link>
               </div>
             </div>
-            <pages :pgnum="pagination" @getpgnum="getproducts" />
+            <pages :pgnum="pagination" @getpgnum="filtersdata" />
           </div>
         </div>
       </div>
@@ -49,8 +58,9 @@ export default {
   data() {
     return {
       custproducts: [],
-      categorys: [],
+      categorys: ["全部商品"],
       pagination: {},
+      optiontext: "",
       isLoading: false,
     };
   },
@@ -61,24 +71,58 @@ export default {
     pages,
   },
   methods: {
-    getproducts(page = 1) {
-      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products?page=${page}`;
+    getproducts() {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/products?page=${pages}`;
       const vm = this;
       vm.isLoading = true;
       //console.log(process.env.APIPATH)
-      this.$http.get(api).then((response) => {
-        console.log(response.data);
+      this.$http.get(api).then((resp) => {
+        console.log(resp.data);
         vm.isLoading = false;
-        vm.custproducts = response.data.products;
-        vm.pagination = response.data.pagination;
-        vm.getoption(response.data.products.category);
+        vm.custproducts = resp.data.products;
+        vm.pagination = resp.data.pagination;
+        vm.getoption(vm.custproducts);
       });
     },
     getoption(element) {
       const vm = this;
-      if (vm.categorys.indexOf(element)==-1){
-        vm.categorys = element;
+      element.forEach(function (el) {
+        if (vm.categorys.indexOf(el.category) === -1) {
+          vm.categorys.push(el.category);
+        }
+      });
+    },
+    changeoption(e) {
+      const vm = this;
+      vm.optiontext = e.target.text;
+      console.log(e.target.text);
+    },
+  },  
+  computed: {
+    filtersdata() {
+      const vm = this;
+      switch (vm.optiontext) {
+        case "全部商品":
+          return vm.custproducts;
+          break;
+        case "":
+          return vm.custproducts;
+          break;
+        default:
+          let filtersproducts = [];
+          vm.custproducts.forEach((item)=>{
+            if(item.category === vm.optiontext){
+              filtersproducts.push(item);
+            }
+
+          })
+          
+          return filtersproducts;
+          break;
       }
+    },
+    filterpage(){
+      return vm.pagination;
     },
   },
 };

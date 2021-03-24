2d4090cbb5e6b36083eccf861b6afbfa83c24e85
harryhsiao <58318398+harryhsiao@users.noreply.github.com>
2021-03-24 21:39:05 +0800

    更換設計

--- src/components/page/homepage.vue
+++ src/components/page/homepage.vue
@@ -1,117 +1,97 @@
 <template>
-  <div>
-    <header>
-      <div
-        class="title text-center d-flex flex-column align-items-center justify-content-center mt-5"
-      >
-        <h1>台味</h1>
-        <div class="line my-5"></div>
-        <p class="h4 title--jr">專門茶葉製所</p>
-      </div>
-    </header>
-    <main>
-      <article class="container">
-        <section class="row">
-          <div class="col-md-5 border-right">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 100.671 112.751"
-            >
-              <polygon
-                fill="#FFFFFF"
-                stroke="#72BE70"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                points="10.067,97.315 16.779,102.014   41.611,86.577 67.786,80.537 92.618,46.309 92.618,15.436 81.879,6.711 75,13.247 68.457,19.463 54.362,29.53 33.557,46.309   27.517,62.416 25.503,69.799 29.53,75.168 32.214,79.195 26.174,84.564 18.121,91.275 "
-              />
-              <line
-                fill="none"
-                stroke="#72BE70"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                x1="43.625"
-                y1="73.154"
-                x2="77.181"
-                y2="29.53"
-              />
-              <line
-                fill="none"
-                stroke="#72BE70"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                x1="60.403"
-                y1="62.416"
-                x2="67.786"
-                y2="63.758"
-              />
-              <line
-                fill="none"
-                stroke="#72BE70"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                x1="71.141"
-                y1="46.98"
-                x2="81.208"
-                y2="44.966"
-              />
-              <line
-                fill="none"
-                stroke="#72BE70"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                x1="51.342"
-                y1="36.913"
-                x2="57.047"
-                y2="44.966"
-              />
-              <line
-                fill="none"
-                stroke="#72BE70"
-                stroke-width="3"
-                stroke-miterlimit="10"
-                x1="36.913"
-                y1="54.362"
-                x2="47.651"
-                y2="54.362"
-              />
-            </svg>
+  <div class="wraper">
+    <section class="s2 btitle">
+      <img
+        class="img-fluid mb-2"
+        src="https://upload.cc/i1/2021/03/17/DuLmv7.png"
+        alt=""
+      />
+      <h3 class="text-white border-top pt-4">台灣特色料理餐廳</h3>
+    </section>
+    <section>
+      <div class="container-fluid pl-0 d-md-flex">
+        <div class="col-md-4 fricephoto"></div>
+        <div class="container">
+          <div class="row text-md-dark">
+            <div class="col-md-8 ml-md-7 mt-md-7">
+              <h2 class="border-left border-maincolor pl-3">關於台味<br />STORY</h2>
+              <p class="text-left pl-md-4 pt-5">
+                世界需要改革，需要對台味餐廳有新的認知。我們都有個共識，若問題很困難，那就勢必不好解決。
+                總而言之，蕭伯納說過一句著名的話，女人無法容忍強加在自己頭上的不貞。這讓我的思緒清晰了。
+                西塞羅講過一段深奧的話，智力享受的方法很多，而且各種各樣，我是指明智而有教養的人，
+                <br class="d-block d-sm-none d-md-block"/>對這樣的人來說生活就是思考。
+                這句話看似簡單，但其中的陰鬱不禁讓人深思。塞涅卡曾經認為，生命如同寓言，其價值不在與長短，而在與內容。
+                這句話決定了一切。台味餐廳絕對是史無前例的。李維在不經意間這樣說過，
+                勞動和享樂這兩個本質上截然不同的東西被一種自然的聯繫結合在一起。希望各位能用心體會這段話。
+                如果仔細思考台味餐廳，會發現其中蘊含的深遠意義。左傳深信，善不可失，惡不可長。
+                這段話看似複雜，其中的邏輯思路卻清晰可見。探討台味餐廳時，如果發現非常複雜，那麼想必不簡單。
+                既然如此，馬克思告訴我們，科學絕不是一種自私自利的享受。有幸能夠致力於科學研究的人，首先應該拿自己的學識為人類服務。
+                這段話令我陷入了沈思。深入的探討台味餐廳，是釐清一切的關鍵。我們要從本質思考，從根本解決問題。
+              </p>
+            </div>
           </div>
-          <div class="col-md-7 border-left"></div>
-        </section>
-        <section class="row">
-          <div class="col-md-5 border-right"></div>
-          <div class="col-md-7 border-left"></div>
-        </section>
-      </article>
-    </main>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
+
+<script>
+export default {
+  directive: {
+    scroll: {
+      inserted(el, binding) {
+        let f = function (evt) {
+          if (binding.value(evt, el)) {
+            window.removeEventListener("scroll", f);
+          }
+        };
+        window.addEventListener("scroll", f);
+      },
+    },
+  },
+  methods: {
+    handleScroll(evt, el) {
+      if (window.scrollY > 50) {
+        el.setAttribute(
+          "style",
+          "opacity: 1; transform: translate3d(0, -10px, 0)"
+        );
+      }
+      return window.scrollY > 100;
+    },
+  },
+};
+</script>
+
 <style scoped>
-header,
-main {
-  height: calc(101vh - 136px - 180px);
-}
-.title--jr {
-  letter-spacing: 105px;
-}
-.line {
-  width: 75%;
-  height: 1px;
-  background-color: #000;
+body {
+  font-family: Montserrat;
 }
 
-.title h1 {
-  font-size: 350px;
-  font-family: PMingLiU;
-  letter-spacing: 135px;
-  transition: cubic-bezier(0.075, 0.82, 0.165, 1);
-}
-.con-h1 {
-  font-size: 120px;
+.btitle {
+  min-height: 100vh;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
-.footer {
-  max-width: 100%;
+.s2 {
+  background-color: #f2f2f2;
+  background-size: cover;
+  background-position: center center;
+  background-image: url("https://images.pexels.com/photos/3123792/pexels-photo-3123792.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+  background-repeat: no-repeat;
+}
+
+.fricephoto {
+  background-image: url("https://images.unsplash.com/photo-1464500542410-1396074bf230?ixlib=rb-1.2.1");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  height: 100vh;
 }
 </style>

fc065f79fc641e86ff7cf4ca6e4534f5cd7f846a
harryhsiao <58318398+harryhsiao@users.noreply.github.com>
2021-04-06 18:33:22 +0800

    1.更改選項內容 2.增加scroll高度變化特效

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg p-5" :class="{'navbar-light':!stylechange,'navbar-dark':stylechange,'bg-white':!stylechange,'bg-transparent':stylechange}">      
+    <nav class="navbar navbar-expand-lg heightback" :class="stylechange">
       <button
         class="navbar-toggler"
         type="button"
@@ -14,61 +14,110 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ml-auto">
-          <router-link class="nav-item nav-link" to="/" href="#">品牌介紹</router-link>
-          <router-link class="nav-item nav-link" to="/cart" href="#">嚴選好茶</router-link>
-          <router-link class="nav-item nav-link" to="/shop" href="#">門市據點</router-link>
-          <router-link class="nav-item nav-link" to="/dashboard/products" href="#" v-show="memberin == 'yes'">客戶中心</router-link>
-          <router-link class="nav-item nav-link" to="/login" href="#" v-show="memberin == 'no'">登入</router-link>
-          <a class="nav-item nav-link" href="#" @click.prevent="logout()" v-show="memberin == 'yes'"
-            >登出</a
+          <router-link class="nav-item nav-link" to="/" href="#"
+            >品牌介紹</router-link
           >
+          <router-link class="nav-item nav-link" to="/cart" href="#"
+            >嚴選好食</router-link
+          >
+          <router-link class="nav-item nav-link" to="/shop" href="#"
+            >門市據點</router-link
+          >
+          <router-link
+            class="nav-item nav-link"
+            to="/login"
+            href="#"
+            v-if="memberin == 'no'"
+            >登入</router-link
+          >
+          <div class="dropdown" v-else>
+            <button
+              id="btnGroupDrop1"
+              type="button"
+              class="btn btn-secondary btn-circle ml-4 mr-5"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i class="fas fa-user"></i>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+              <router-link
+                class="dropdown-item"
+                to="/dashboard/products"
+                href="#"
+                >客戶中心</router-link
+              >
+              <a class="dropdown-item" href="#" @click.prevent="logout()"
+                >登出</a
+              >
+            </div>
+          </div>
         </div>
       </div>
     </nav>
   </div>
 </template>
 
+<style scoped>
+.btn-circle {
+  border-radius: 50px;
+}
+
+.heightback{
+  transition: padding ease-in-out 0.5s;
+}
+</style>
+
 <script>
 export default {
   data() {
     return {
-      memberin: 'no',
-      open:true,
+      memberin: "no",
+      open: true,
+      windowTop: 0,
     };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
   },
   created() {
     let api = `${process.env.APIPATH}/api/user/check`;
-    let vm = this; 
+    let vm = this;
     vm.$http.post(api).then((response) => {
       console.log(response.data);
       if (response.data.success) {
-
-        vm.memberin = 'yes';
-
+        vm.memberin = "yes";
       } else {
-        vm.memberin = 'no';
+        vm.memberin = "no";
       }
-    });    
+    });
   },
-  computed:{
-    stylechange(){
+  computed: {
+    stylechange() {
       console.log(this.$route.path);
       const vm = this;
       let homePath = vm.$route.path;
       switch (homePath) {
-        case '/':
-          return true;
+        case "/dashboard/products":
+          return "navbar-light bg-white p-5";
           break;
-        case '/cart':
-          return true;
-          break;         
+        case "/":
+          if (vm.windowTop > 30) {
+            return "navbar-dark bg-dark fixed-top p-lg-2";
+          } else {
+            return "navbar-dark bg-dark fixed-top p-lg-5";
+          }
+          break;
         default:
-          return false;
+          return "navbar-dark bg-dark p-5";
           break;
       }
       console.log(vm.open);
-      
-    }
+    },
   },
   methods: {
     logout() {
@@ -77,14 +126,15 @@ export default {
       this.$http.post(api).then((response) => {
         console.log(response.data);
         if (response.data.success) {
-          vm.$router.push("/homepage");
+          vm.$router.push("/");
         }
       });
-    },     
-    
-  }, 
-   
+    },
+    onScroll(e) {
+      const vm = this;
+      vm.windowTop =
+        window.top.scrollY; /* or: e.target.documentElement.scrollTop */
+    },
+  },
 };
-
-
 </script>

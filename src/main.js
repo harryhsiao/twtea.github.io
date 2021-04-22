b366b37cea70528a04c1cf08bd4e44c499a7db18
harryhsiao <58318398+harryhsiao@users.noreply.github.com>
2021-04-22 20:37:52 +0800

    新增套件 vee-validate

--- src/main.js
+++ src/main.js
@@ -1,29 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import VeeValidate from 'vee-validate';
+import zhTwValidate from 'vee-validate/dist/locale/zh_TW';
 
-import 'bootstrap'
+import 'bootstrap';
 //import { library } from '@fortawesome/fontawesome-svg-core'
 //import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 //import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import Loading from 'vue-loading-overlay'
-import 'vue-loading-overlay/dist/vue-loading.css'
-import { firestorePlugin } from 'vuefire'
-import App from './App'
-import router from './router'
-import './bus'
-import currencyFilter from './filters/currency'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import { firestorePlugin } from 'vuefire';
+import App from './App';
+import router from './router';
+import './bus';
+import currencyFilter from './filters/currency';
 
-Vue.component('Loading', Loading)
+Vue.component('Loading', Loading);
 //Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.use(firestorePlugin)
+Vue.use(firestorePlugin);
 //library.add(faUserSecret)
-
 
 axios.defaults.withCredentials = true;
 Vue.config.productionTip = false;
+VeeValidate.Validator.localize('zh_TW', zhTwValidate);
+Vue.use(VeeValidate);
 Vue.use(VueAxios, axios);
 Vue.filter('currency', currencyFilter);
 

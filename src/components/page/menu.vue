d1f033346dfd0ee99d4ea3653215dc98a3d283bb
harryhsiao <58318398+harryhsiao@users.noreply.github.com>
2021-05-13 21:22:21 +0800

    新增頁面

---
+++ src/components/page/menu.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="container my-5 p-2 p-md-5 border border-maincolor">
+      <div class="card p-3 border border-maincolor">
+        <div class="card-body">
+          <h2 class="border-left border-maincolor ml-3 pl-3">
+            本店菜單<br />Menu
+          </h2>
+          <ul
+            class="price-style menu-content mx-auto d-lg-block d-none text-primary"
+          >
+            <li class="mx-4">牛肉炒芥藍&nbsp; <span>350</span>元</li>
+            <li class="mx-4">三層炒蒜苗&nbsp; <span>100</span>元</li>
+            <li class="mx-4">金莎嫩豆腐&nbsp; <span>250</span>元</li>
+            <li class="mx-4">黃金烏魚子&nbsp; <span>750</span>元</li>
+            <li class="mx-4">螃蟹豆腐煲&nbsp; <span>450</span>元</li>
+            <li class="mx-4">古早味封肉&nbsp; <span>350</span>元</li>
+            <li class="mx-4">純味蘿蔔糕&nbsp; <span>250</span>元</li>
+            <li class="mx-4">薑絲鮮魚湯&nbsp; <span>350</span>元</li>
+            <li class="mx-4">蒜蓉海戰車&nbsp; <span>時&nbsp;價</span></li>
+            <li class="mx-4">以及其他菜色&nbsp;．．．</li>
+          </ul>
+          <ul class="price-style d-lg-none d-block text-primary">
+            <li class="my-4">牛肉炒芥藍&nbsp; <span>350</span>元</li>
+            <li class="my-4">三層炒蒜苗&nbsp; <span>100</span>元</li>
+            <li class="my-4">金莎嫩豆腐&nbsp; <span>250</span>元</li>
+            <li class="my-4">黃金烏魚子&nbsp; <span>750</span>元</li>
+            <li class="my-4">螃蟹豆腐煲&nbsp; <span>450</span>元</li>
+            <li class="my-4">古早味封肉&nbsp; <span>350</span>元</li>
+            <li class="my-4">純味蘿蔔糕&nbsp; <span>250</span>元</li>
+            <li class="my-4">薑絲鮮魚湯&nbsp; <span>350</span>元</li>
+            <li class="my-4">蒜蓉海戰車&nbsp; <span>時&nbsp;價</span></li>
+            <li class="my-4">以及其他菜色&nbsp;．．．</li>
+          </ul>
+          <p
+            class="text-center"
+            style="line-height: 2rem; letter-spacing: 0.1rem"
+          >
+            30人以上團體請先預約<br />本店亦有販售自製料理包,請來電預約<br />或參考網路商店
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.menu-content {
+  writing-mode: vertical-rl;
+  font-size: 2rem;
+}
+
+.price-style {
+  list-style: none;
+}
+
+.price-style li span {
+  writing-mode: horizontal-tb;
+  color: tomato;
+}
+</style>
--- src/components/page/shop.vue
+++ src/components/page/shop.vue
@@ -1,39 +1,149 @@
 <template>
   <div>
-    <section>
-      <div class="container-fluid pl-0 d-md-flex">
-        <div class="col-md-5">
-          <iframe
-          class="mymap"
-            src="https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q=台南市東區前鋒路210號&z=16&output=embed&t="
-          ></iframe>
-        </div>
-        <div class="container">
-          <div class="row text-md-dark">
-            <div class="col-md-8 ml-md-7 mt-md-7">
-              <h2 class="border-left border-maincolor pl-3">
-                實體據點<br />LOCATION
-              </h2>
-              <p class="text-left pl-md-4 pt-5">
-                  <ul class="list-group list-group-flush">
-                      <li class="list-group-item border-0 border-0 border-0 border-0 border-0"><i class="fas fa-phone-square-alt"></i>&nbsp;&nbsp;電話:&nbsp;(06)000000</li>
-                      <li class="list-group-item border-0 border-0 border-0 border-0"><i class="fab fa-twitter-square"></i>&nbsp;&nbsp;推特:&nbsp;@ abcdefg</li>
-                      <li class="list-group-item border-0 border-0 border-0"><i class="fab fa-facebook"></i>&nbsp;&nbsp;粉絲團:&nbsp;abcdefg</li>
-                      <li class="list-group-item border-0 border-0"><i class="fas fa-map-marker-alt"></i>&nbsp;&nbsp;店面地址:&nbsp;台南市東區前鋒路XXX號</li>
-                      <li class="list-group-item border-0 text-center">超過30人以上團體請來電預約</li>
-                    </ul>
-              </p>
+    <section class="section">
+      <div class="container">
+        <h2 class="text-center mx-auto my-4">實體店面<br />FIND US</h2>
+        <div class="oldmapbg bgimage my-4" style="height: 10vh"></div>
+        <div class="row">
+          <div class="col-md-4 mb-3">
+            <h5 class="text-center font-weight-bold">
+              台南前鋒店&nbsp;<span class="font-weight-normal">( 總店 )</span>
+            </h5>
+            <div class="card border-0">
+              <img
+                src="https://images.unsplash.com/photo-1528698827591-e19ccd7bc23d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"
+                alt=""
+                class="img-fluid"
+              />
+              <div class="card-body d-flex px-0">
+                <div class="shopinfo-zhtw w-50">
+                  <p>TEL&nbsp;:&nbsp;&nbsp;(06)000000</p>
+                  <p>11:00 - 21:00</p>
+                  <p>台南市東區<br />前鋒路XXX號1樓</p>
+                </div>
+                <div class="photobox w-50">
+                  <iframe
+                    class="mymap"
+                    src="https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q=台南市東區前鋒路210號&z=16&output=embed&t="
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <h5 class="text-center font-weight-bold">新北泰林店</h5>
+            <div class="card border-0">
+              <img
+                src="https://images.unsplash.com/photo-1464869372688-a93d806be852?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+                alt=""
+                class="img-fluid"
+              />
+              <div class="card-body d-flex px-0">
+                <div class="shopinfo-zhtw w-50">
+                  <p>TEL&nbsp;:&nbsp;&nbsp;(02)000000</p>
+                  <p>12:00 - 22:00</p>
+                  <p>新北市泰山區<br />泰林路二段XXX號 B1</p>
+                </div>
+                <div class="photobox w-50">
+                  <iframe
+                    class="mymap"
+                    src="https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q=新北市泰山區泰林路二段240號&z=16&output=embed&t="
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <h5 class="text-center font-weight-bold">台北信義店</h5>
+            <div class="card border-0">
+              <img
+                src="https://images.unsplash.com/photo-1542581482-851c38ee4b94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                alt=""
+                class="img-fluid"
+              />
+              <div class="card-body d-flex px-0">
+                <div class="shopinfo-zhtw w-50">
+                  <p>TEL&nbsp;:&nbsp;&nbsp;(02)000000</p>
+                  <p>11:00 - 24:00</p>
+                  <p>台北市信義區<br />仁愛路四段XXX號 2樓</p>
+                </div>
+                <div class="photobox w-50">
+                  <iframe
+                    class="mymap"
+                    src="https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q=台北市信義區仁愛路四段240號&z=16&output=embed&t="
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <h5 class="text-center font-weight-bold">羅東傳藝店</h5>
+            <div class="card border-0">
+              <img
+                src="https://images.unsplash.com/photo-1518739745383-0ef26e9dd7fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80"
+                alt=""
+                class="img-fluid"
+              />
+              <div class="card-body d-flex px-0">
+                <div class="shopinfo-zhtw w-50">
+                  <p>TEL&nbsp;:&nbsp;&nbsp;(03)000000</p>
+                  <p>
+                    12:00 - 20:00<br />
+                    例假日 - 21:00
+                  </p>
+                  <p>宜蘭縣羅東鎮<br />傳藝路二段XXX號 1樓</p>
+                </div>
+                <div class="photobox w-50">
+                  <iframe
+                    class="mymap"
+                    src="https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q=宜蘭縣羅東鎮傳藝路二段20號&z=16&output=embed&t="
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <h5 class="text-center font-weight-bold">高雄前鎮店</h5>
+            <div class="card border-0">
+              <img
+                src="https://images.unsplash.com/photo-1534986136417-89b28be6a379?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                alt=""
+                class="img-fluid"
+              />
+              <div class="card-body d-flex px-0">
+                <div class="shopinfo-zhtw w-50">
+                  <p>TEL&nbsp;:&nbsp;&nbsp;(07)000000</p>
+                  <p>
+                    12:00 - 22:00<br />
+                  </p>
+                  <p>高雄市前鎮區<br />擴建路XXX號</p>
+                </div>
+                <div class="photobox w-50">
+                  <iframe
+                    class="mymap"
+                    src="https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q=高雄市前鎮區擴建路109號&z=16&output=embed&t="
+                  ></iframe>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </section>   
+    </section>
   </div>
 </template>
 
 <style scoped>
 .mymap {
   width: 100%;
-  height: 71.6vh;
+  border: none;
+}
+.bgimage {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+}
+.oldmapbg {
+  background-image: url("http://www.openluck.net/wp-content/gallery/tainanstory/thumbs/thumbs_tainan001.jpg");
 }
 </style>

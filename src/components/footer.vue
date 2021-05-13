003957efbc94769a74baed82380d37b68535b712
harryhsiao <58318398+harryhsiao@users.noreply.github.com>
2021-05-13 21:20:13 +0800

    更改 html 標籤名稱

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="">
-    <div class="footer text-center py-5 bg-maincolor">
+    <footer class="footer text-center py-4 bg-maincolor">
       <h6>練習用網頁 | 不具商業用途</h6>
-    </div>
+    </footer>
   </div>
 </template>
 

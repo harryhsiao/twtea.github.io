2877ec7c0b2c192df7d436cb93abd4805d649cba
harryhsiao <58318398+harryhsiao@users.noreply.github.com>
2021-06-10 23:30:32 +0800

    全覆蓋載入畫面

--- src/components/page/menu.vue
+++ src/components/page/menu.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <loading :active.sync="isLoading"></loading>
+    <loading :active.sync="isLoading" class="bg-bwood w-100 fullpg-bgcover loadopacity">
+      <img
+        class="img-fluid mx-auto d-block"
+        src="https://upload.cc/i1/2021/03/17/DuLmv7.png"
+        alt="logo"
+        style="height: 90px"
+      />
+      <h3 class="text-white border-top pt-4">台灣特色料理餐廳</h3>
+    </loading>
     <section class="py-4 fricephoto bgimage">
       <div class="container p-2 p-md-5 border border-maincolor">
         <div class="card p-3 border border-maincolor">
@@ -12,7 +20,13 @@
               style="height: 90px"
             />
             <ul
-              class="price-style menu-content mx-auto d-lg-block d-none text-primary"
+              class="
+                price-style
+                menu-content
+                mx-auto
+                d-lg-block d-none
+                text-primary
+              "
             >
               <li class="mx-4">牛肉炒芥藍&nbsp; <span>350</span>元</li>
               <li class="mx-4">三層炒蒜苗&nbsp; <span>100</span>元</li>
@@ -26,7 +40,12 @@
               <li class="mx-4">以及其他菜色&nbsp;．．．</li>
             </ul>
             <ul
-              class="price-style d-lg-none d-block text-primary text-center pl-0"
+              class="
+                price-style
+                d-lg-none d-block
+                text-primary text-center
+                pl-0
+              "
             >
               <li class="my-4">牛肉炒芥藍&nbsp; <span>350</span>元</li>
               <li class="my-4">三層炒蒜苗&nbsp; <span>100</span>元</li>
@@ -77,6 +96,11 @@
 </template>
 
 <style scoped>
+
+.loadopacity {
+  transition: opacity ease-in-out 0.3s;
+}
+
 .menu-content {
   writing-mode: vertical-rl;
   font-size: 2rem;
@@ -85,6 +109,10 @@
 .thumbnail:hover {
   transition: box-shadow ease-in-out 0.3s;
   box-shadow: 0 1rem 1rem rgb(0 0 0 / 8%);
+}
+
+.fullpg-bgcover {
+  transition: background-color ease-in-out 0.3s;
 }
 
 .price-style {
@@ -103,6 +131,9 @@ export default {
     return {
       custproducts: [],
       isLoading: false,
+      status: {
+        fileuploading: false,
+      },
     };
   },
   created() {
@@ -115,7 +146,9 @@ export default {
       vm.isLoading = true;
       this.$http.get(api).then((resp) => {
         console.log(resp.data);
-        vm.isLoading = false;
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 3000);
         vm.custproducts = resp.data.products;
       });
     },

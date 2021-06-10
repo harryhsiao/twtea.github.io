b382555b0607b4893603f99b07b554881e398847
harryhsiao <58318398+harryhsiao@users.noreply.github.com>
2021-06-10 23:27:26 +0800

    覆蓋整個頁面的 載入畫面

---
+++ src/components/kit/fullpgLoad.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    <div class="bg-maincolor w-100 fullpg-bgcover" role="status">
+      <img
+        class="img-fluid mb-2"
+        src="https://upload.cc/i1/2021/03/17/DuLmv7.png"
+        alt=""
+        style="height: 100px"
+      />
+      <h3 class="text-white border-top pt-4">台灣特色料理餐廳</h3>
+    </div>
+  </div>
+</template>

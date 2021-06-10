558dd75418271e30932abe86adda2641e43bca6b
harryhsiao <58318398+harryhsiao@users.noreply.github.com>
2021-06-10 23:28:14 +0800

    sider RWD收合

--- src/components/template/sidebar.vue
+++ src/components/template/sidebar.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
     <nav
-      class="navbar navbar-dark fixed-top bg-maincolor flex-md-nowrap p-0 shadow"
+      class="
+        navbar navbar-dark
+        fixed-top
+        bg-maincolor
+        flex-md-nowrap
+        p-0
+        shadow
+      "
     >
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#/">回到主頁</a>
+      <button
+        class="navbar-brand col-sm-3 col-md-2 mr-0 d-inline d-md-none"
+        @click="onsider"
+      >
+        管理員項目
+        <i class="fas fa-bars"></i>
+      </button>
     </nav>
-    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+    <nav class="col-md-2 bg-light sidebar" :class="{ showSidebar: tran,hideSidebar: !tran }">
       <div class="sidebar-sticky">
         <h6
-          class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
+          class="
+            sidebar-heading
+            d-flex
+            justify-content-between
+            align-items-center
+            px-3
+            mt-4
+            mb-1
+            text-muted
+          "
         >
           <span class="h2">管理員項目</span>
           <a class="d-flex align-items-center text-muted" href="#">
@@ -16,25 +39,25 @@
           </a>
         </h6>
         <ul class="nav flex-column">
-          <li class="nav-item border">
+          <li class="nav-item border py-4">
             <router-link class="nav-link h4" to="/dashboard/products"
               ><i class="fas fa-box-open fa-fw"></i
               >&nbsp;&nbsp;產品列表</router-link
             >
           </li>
-          <li class="nav-item border">
+          <li class="nav-item border py-4">
             <router-link class="nav-link h4" to="/dashboard/orders"
               ><i class="fas fa-clipboard-list fa-fw"></i
               >&nbsp;&nbsp;訂單列表</router-link
             >
           </li>
-          <li class="nav-item border">
+          <li class="nav-item border py-4">
             <router-link class="nav-link h4" to="/dashboard/coupon"
               ><i class="fas fa-ticket-alt fa-fw"></i
               >&nbsp;&nbsp;優惠券</router-link
             >
           </li>
-          <li class="nav-item border">
+          <li class="nav-item border py-4">
             <a class="nav-link h4" href="#" @click.prevent="logout()"
               ><i class="fas fa-sign-in-alt"></i>&nbsp;&nbsp;登出</a
             >
@@ -46,10 +69,6 @@
 </template>
 
 <style scoped>
-/*
- * Sidebar
- */
-
 .sidebar {
   position: fixed;
   top: 0;
@@ -58,6 +77,15 @@
   z-index: 100; /* Behind the navbar */
   padding: 48px 0 0; /* Height of navbar */
   box-shadow: inset -1px 0 0 rgba(0, 0, 0, 0.1);
+  transition: transform ease-in-out 0.5s;
+}
+
+.hideSidebar {
+  transform: translate(-100%);
+}
+
+.showSidebar {
+  transform: translate(0%);
 }
 
 .sidebar-sticky {
@@ -103,6 +131,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      tran: false,
+    };
+  },
   methods: {
     logout() {
       let api = `${process.env.APIPATH}/logout`;
@@ -113,6 +146,10 @@ export default {
           vm.$router.push("/");
         }
       });
+    },
+    onsider() {
+      const vm = this;
+      vm.tran = !vm.tran;
     },
   },
 };
